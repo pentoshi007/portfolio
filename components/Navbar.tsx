1ea2bc82b4a609691851d279a0994dfc7d7a9822
Aniket Pandey <aniket00736@gmail.com>
2026-01-22 21:37:16 +0530

    style: reset navbar links to default position

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
             </div>
           </a>
 
-          <div className="hidden md:flex items-center gap-1 mr-[7%]">
+          <div className="hidden md:flex items-center gap-1">
             {navItems.map((item, i) => (
               <a
                 key={item.label}
